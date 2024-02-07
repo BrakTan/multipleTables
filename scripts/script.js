3243f102319c04cad67f7767fdf98811c3c89255
Tanguy Leconte <taleconte@gmail.com>
2024-02-07 12:43:57 +0100

    Finished exercices and and a start button

--- scripts/script.js
+++ scripts/script.js
@@ -2,36 +2,69 @@ let minuteur = document.querySelector('.minuteur');
 let nextButton = document.querySelector('.nextButton');
 let tableButton = document.querySelectorAll('.table');
 let exercice = document.querySelector('.exercice');
+let scoreShow = document.querySelector('.score');
+let tableValue = 0;
+
 let score = 0;
 let timeRemaining = 0;
 let timeInterval;
 
 tableButton.forEach((table) => {
-    let tableValue = table.getAttribute('value');
     table.addEventListener('click', function() {
-        let multiple = Math.ceil(Math.random()*10);
-        exercice.innerHTML = tableValue+'X'+multiple;
-        launchTimer();
+        tableValue = table.getAttribute('value');
+        nextButton.innerHTML = 'Commencer';
+        exercice.innerHTML = 'Prêt ?';
+        score = -1;
+        nextButton.removeAttribute('disabled');
+        scoreShow.innerHTML = 'Score : 0';
     })
 })
 
-nextButtonButton.addEventListener("click", function() {
+nextButton.addEventListener("click", function() {
+    if(score == -1){
+        runExercice();
+        nextButton.innerHTML = 'Suivant->';
+    }
     score++;
+    scoreShow.innerHTML = 'Score : '+score;
+    console.log(tableValue);
+    let order = Math.round(Math.random()*2);
+    let multiple = Math.ceil(Math.random()*10);
+    exercice.innerHTML = order <= 1 ? tableValue+'X'+multiple : multiple+'X'+tableValue;
 });
 
+function runExercice(){
+    let order = Math.round(Math.random()*2);
+    let multiple = Math.ceil(Math.random()*10);
+    exercice.innerHTML = order <= 1 ? tableValue+'X'+multiple : multiple+'X'+tableValue;
+    nextButton.removeAttribute('disabled');
+    launchTimer();
+}
 
 function launchTimer(){
     if(timeRemaining > 0){
         clearInterval(timeInterval);
     }
     timeRemaining = 60;
+    minuteur.setAttribute('style', 'color: black');
     minuteur.innerHTML = ':60'
     function time(){
     timeRemaining--;
-    console.log(timeRemaining);
+    //console.log(timeRemaining);
     minuteur.innerHTML = ':'+timeRemaining;
+    if(timeRemaining <= 10){
+        minuteur.setAttribute('style', 'color: red');
+    }
     if(timeRemaining <= 0){
         clearInterval(timeInterval);
+        nextButton.setAttribute('disabled', '');
+        if(score > 0){
+            exercice.innerHTML = 'Bravo ! Ton score est de '+score; 
+        }
+        else{
+            exercice.innerHTML = 'Ton score est de 0';
+        }
+
     };
 };
 timeInterval = setInterval(time, 1000);
